fbe9775dcad1cae3450f9af8f006b883c15a3533
Sam Zajczenko <113551869+Sam786968@users.noreply.github.com>
2022-10-17 20:02:13 +0000

    all work done

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ game_num = randint(1,100)
 print(f"Well {name}, I've thought of a number between 1 and 100 and you have only eight tries to guess is. What number do you think it is? ")
 print("-------------------------------------------")
 
-tries = 1
+tries = 0
 
 for i in range(8):
   tries += 1
@@ -25,9 +25,9 @@ for i in range(8):
     print(f"Congratuations you got the right one. It took you {i + 1} times.")
     print("-------------------------------------------")
     break
-  elif tries == 8:
-    print(f"You were wrong 8 times the secret number was {game_num}.")
-    print("-------------------------------------------")
   else:
     print("You are wrong, try again.")
+    print("-------------------------------------------")
+  if tries == 8:
+    print(f"You were wrong 8 times the secret number was {game_num}.")
     print("-------------------------------------------")
